bedebcbefc5a180ee9c61257235458512b3d17be
anbreaker <antunez19@gmail.com>
2020-01-08 21:09:20 +0100

    avanando en el curso de flask

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-#decorador de python
+toDos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+
 @app.route('/')
-def hello():
+def index():
     userIp = request.remote_addr
-    return f'Hello world Flask tu ip es: {userIp}'
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('userIp', userIp)
+
+    return response
+
+# decorador de python
+@app.route('/hello')
+def hello():
+    userIp = request.cookies.get('userIp')
+    context = {
+        'userIp': userIp,
+        'toDos': toDos
+    }
+
+    # return f'Hello world Flask tu ip es: {userIp}'
+    return render_template('/hello.html', **context)
