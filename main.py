fb443c5c7439e465636b8a5851c726dde9516bf2
anbreaker <antunez19@gmail.com>
2020-01-21 18:58:42 +0100

    Agregando macros y templates base

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-toDos = ['TODO 1', 'TODO 2', 'TODO 3']
+toDos = ['Tarea 1', 'Tarea 2', 'Tarea 3']
 
 
 @app.route('/')
