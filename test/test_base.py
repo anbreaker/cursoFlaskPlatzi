3ba4c7d0f239d999089fe586795e9fe86ce11349
anbreaker <antunez19@gmail.com>
2020-01-28 00:31:37 +0100

    test con get y post, probando forms

--- test/test_base.py
+++ test/test_base.py
@@ -1,5 +1,5 @@
 from flask_testing import TestCase
-from flask import current_app
+from flask import current_app, url_for
 
 from main import app
 
@@ -13,3 +13,23 @@ class MainTest(TestCase):
 
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
+        
+    def test_app_in_test_mode(self):
+        self.assertTrue(current_app.config['TESTING'])
+        
+    def test_index_redirects(self):
+        response = self.client.get(url_for('index'))
+        self.assertRedirects(response, url_for('hello'))
+        
+    def test_hello_get(self):
+        response = self.client.get(url_for('hello'))
+        self.assert200(response)
+    
+    def test_hello_post(self):
+        fake_form = {
+            'userName': 'fake',
+            'password': 'fake-password'
+        }
+        response = self.client.post(url_for('hello'), data=fake_form)
+
+        self.assertRedirects(response, url_for('index'))
