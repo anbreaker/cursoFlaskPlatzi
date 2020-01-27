3ec952bed0bb5a5ebb3a395ad6b8e80dc8a8aa69
anbreaker <antunez19@gmail.com>
2020-01-27 09:52:21 +0100

    agregando bootstrap a flask

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 toDos = ['Tarea 1', 'Tarea 2', 'Tarea 3']
 
